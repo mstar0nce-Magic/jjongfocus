e0b90a4c7e5dfe3d908c3a9d24b61c1bbcc8483f
mstar0nce-Magic <mstar0nce@gmail.com>
2026-09-12 22:28:34 +0900

    Refresh cache for Enter key password fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jjongfocus-v0.1.7.2';
+const CACHE='jjongfocus-v0.1.7.3';
 const ASSETS=['./','./index.html','./styles.css','./diary.css','./app.js','./diary.js','./timeline2.js','./record-reset.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
