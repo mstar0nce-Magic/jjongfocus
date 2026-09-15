5db24b6f54df72836b845e0a061bfe23219cf39a
mstar0nce-Magic <mstar0nce@gmail.com>
2026-09-15 19:57:09 +0900

    Cache recovery flow for v0.2.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='jjongfocus-v0.2.0';
-const ASSETS=['./','./index.html','./styles.css','./diary.css','./app.js','./diary.js','./timeline2.js','./record-reset.js','./version-fix.js','./calendar.js','./focus20.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='jjongfocus-v0.2.1';
+const ASSETS=['./','./index.html','./styles.css','./diary.css','./recovery.css','./app.js','./diary.js','./timeline2.js','./record-reset.js','./version-fix.js','./calendar.js','./focus20.js','./recovery.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
